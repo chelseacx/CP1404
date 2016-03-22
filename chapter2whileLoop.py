68a648e87eaad4db0d0769a0fa87ebba6dad2f33
chelseacx <chelsea.chen@my.jcu.edu.au>
2016-03-22 11:18:06 +0800

    chapter2whileLoop.py

--- chapter2whileLoop.py
+++ chapter2whileLoop.py
@@ -1,4 +1,4 @@
 x = 0
-while x < 10: #starting condition
-    print("Now my x is ", x)
-    x += 1 #increments by 1 after each iteration
+while x < 10: # starting condition
+    print("Now my x is ", x) # executed when while is true
+    x += 1 # increments by 1 after each iteration
